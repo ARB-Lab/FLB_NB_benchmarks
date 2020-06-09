f9928ce8b26a3db3d7abb7a1d6d229a5ad27a7eb
axelvonkamp <47664585+axelvonkamp@users.noreply.github.com>
2020-06-09 09:58:13 +0200

    input files for the benchmark function

---
+++ FLB_NB_benchmark_examples.m
@@ -0,0 +1,19 @@
+% the examples below are for the C. glutamicum iJM658 model
+%%
+% enumerate synthetic lethals up to size 5 for mue threshold subselection 1,
+% indicators, split v, combined z and irrev_geq
+% runs only the FLB approach because irrev_geq is not supported by NB
+FLB_NB_benchmarks('iJM658_synthetic_lethals_input', 1, 1, 5, 5, 0, 1, 1, true, true, true, 1e-6, 1e-6, 1e-7, 72000)
+
+%%
+% enumerate synthetic lethals up to size 5 for mue threshold subselection 1,
+% indicators, split v, separate z and without irrev_geq
+% runs the FLB and NB approach
+FLB_NB_benchmarks('iJM658_synthetic_lethals_input', 2, 2, 5, 5, 0, 1, 1, true, false, false, 1e-6, 1e-6, 1e-7, 72000)
+
+%%
+% enumerate MCS up to size 8 for species index subselection 38 with seed 5,
+% indicators, split v, combined z and irrev_geq
+% runs only the FLB approach (stoichmat and kernel desired) because
+% irrev_geq is not supported by NB
+FLB_NB_benchmarks('iJM658_mcs_input', 38, 38, 5, 8, 0, 1, 1, true, true, true, 1e-6, 1e-6, 1e-7, 72000)
---
+++ FLB_NB_benchmarks.m
@@ -0,0 +1,127 @@
+function FLB_NB_benchmarks(input_file, first, last, seed, max_sz, bigM, threshold, split_level,...
+  reduce_constraints, combined_z, irrev_geq, opt_tol, feas_tol, int_tol, time_limit)
+% this function runs the MCS enumerations with the main MILP inputs
+% together with the selected indices being read from input_file
+% first:last specifies the subselection of indices for which the benchmarks are run
+% seed: CPLEX random seed
+% max_sz: enumeration is performed with successive poulate calls up to MCS size max_sz
+% bigM, threshold, split_level, reduce_constraints, combined_z, irrev_geq:
+% paramaters for ConstrainedMinimalCutSetsEnumeratorMILP
+% opt_tol, feas_tol, int_tol: CPLEX tolerance settings
+% time_limit: time limit in seconds for each MCS size level
+
+solution_limit= Inf;
+working_memory= 80000; % GB
+load([input_file, '.mat'], 'rd_rat', 'irrev_rd_rat', 'flux_lb', 'flux_ub', 'cuts', 'kn',...
+  'idx', 'inh', 'ub', 'des', 'db');
+q= length(rd_rat);
+comp_time= zeros(max_sz, q);
+comp_time2= zeros(max_sz, q);
+comp_time3= zeros(max_sz, q);
+comp_time4= zeros(max_sz, q);
+mcs= cell(1, q);
+mcs2= cell(1, q);
+mcs3= cell(1, q);
+mcs4= cell(1, q);
+matching_mcs= false(1, q);
+
+%%
+for i= idx(first:last)
+  i
+  rd= rd_rat{i};
+  irrev_rd= irrev_rd_rat{i};
+  
+  % FLB stoichmat desired
+  obj= ConstrainedMinimalCutSetsEnumeratorMILP(rd, irrev_rd, [], inh{i}, ub{i}, cuts{i},...
+    flux_lb{i}, flux_ub{i}, des{i}, db{i}, [], bigM, threshold, split_level,...
+    reduce_constraints, combined_z, irrev_geq);
+  obj.cpx.Param.emphasis.numerical.Cur= 1;
+  obj.cpx.Param.workmem.Cur= working_memory;
+  obj.cpx.Param.simplex.tolerances.optimality.Cur= opt_tol;
+  obj.cpx.Param.simplex.tolerances.feasibility.Cur= feas_tol;
+  obj.cpx.Param.mip.tolerances.integrality.Cur= int_tol;
+  obj.cpx.Param.randomseed.Cur= seed;
+  obj.cpx.Param.output.clonelog.Cur= -1;
+  obj.ev_size_lb= 1;
+  for sz = 1:max_sz
+    [obj, mcs_sz, ~, comp_time(sz, i)]= shortest_minimal_cut_sets(obj, sz, solution_limit, time_limit, 2);
+    mcs{i}= [mcs{i}, mcs_sz];
+  end
+  mcs{i}= sparse(sortrows(logical(mcs{i}'))');
+
+  if ~isempty(des{i})
+    % FLB kernel desired
+    obj= ConstrainedMinimalCutSetsEnumeratorMILP(rd, irrev_rd, {kn{i}, 0, 1}, inh{i}, ub{i}, cuts{i},...
+      flux_lb{i}, flux_ub{i}, des{i}, db{i}, [], bigM, threshold, split_level,...
+      reduce_constraints, combined_z, irrev_geq);
+    obj.cpx.Param.emphasis.numerical.Cur= 1;
+    obj.cpx.Param.workmem.Cur= working_memory;
+    obj.cpx.Param.simplex.tolerances.optimality.Cur= opt_tol;
+    obj.cpx.Param.simplex.tolerances.feasibility.Cur= feas_tol;
+    obj.cpx.Param.mip.tolerances.integrality.Cur= int_tol;
+    obj.cpx.Param.randomseed.Cur= seed;
+    obj.cpx.Param.output.clonelog.Cur= -1;
+    obj.ev_size_lb= 1;
+    for sz = 1:max_sz
+      [obj, mcs_sz, ~, comp_time2(sz, i)]= shortest_minimal_cut_sets(obj, sz, solution_limit, time_limit, 2);
+      mcs2{i}= [mcs2{i}, mcs_sz];
+    end
+    mcs2{i}= sparse(sortrows(logical(mcs2{i}'))');
+  end
+
+  if ~irrev_geq % not supported by NB approach
+    % NB stoichmat desired
+    obj= ConstrainedMinimalCutSetsEnumeratorMILP(rd, irrev_rd, {kn{i}, 1, 0}, inh{i}, ub{i}, cuts{i},...
+      flux_lb{i}, flux_ub{i}, des{i}, db{i}, [], bigM, threshold, split_level,...
+      reduce_constraints, combined_z, irrev_geq);
+    obj.cpx.Param.emphasis.numerical.Cur= 1;
+    obj.cpx.Param.workmem.Cur= working_memory;
+    obj.cpx.Param.simplex.tolerances.optimality.Cur= opt_tol;
+    obj.cpx.Param.simplex.tolerances.feasibility.Cur= feas_tol;
+    obj.cpx.Param.mip.tolerances.integrality.Cur= int_tol;
+    obj.cpx.Param.randomseed.Cur= seed;
+    obj.cpx.Param.output.clonelog.Cur= -1;
+    obj.ev_size_lb= 1;
+    for sz = 1:max_sz
+      [obj, mcs_sz, ~, comp_time3(sz, i)]= shortest_minimal_cut_sets(obj, sz, solution_limit, time_limit, 2);
+      mcs3{i}= [mcs3{i}, mcs_sz];
+    end
+    mcs3{i}= sparse(sortrows(logical(mcs3{i}'))');
+    
+    if ~isempty(des{i})
+      % NB kernel desired
+      obj= ConstrainedMinimalCutSetsEnumeratorMILP(rd, irrev_rd, {kn{i}, 1, 1}, inh{i}, ub{i}, cuts{i},...
+        flux_lb{i}, flux_ub{i}, des{i}, db{i}, [], bigM, threshold, split_level,...
+        reduce_constraints, combined_z, irrev_geq);
+      obj.cpx.Param.emphasis.numerical.Cur= 1;
+      obj.cpx.Param.workmem.Cur= working_memory;
+      obj.cpx.Param.simplex.tolerances.optimality.Cur= opt_tol;
+      obj.cpx.Param.simplex.tolerances.feasibility.Cur= feas_tol;
+      obj.cpx.Param.mip.tolerances.integrality.Cur= int_tol;
+      obj.cpx.Param.randomseed.Cur= seed;
+      obj.cpx.Param.output.clonelog.Cur= -1;
+      obj.ev_size_lb= 1;
+      for sz = 1:max_sz
+        [obj, mcs_sz, ~, comp_time4(sz, i)]= shortest_minimal_cut_sets(obj, sz, solution_limit, time_limit, 2);
+        mcs4{i}= [mcs4{i}, mcs_sz];
+      end
+      mcs4{i}= sparse(sortrows(logical(mcs4{i}'))');
+    end
+  end
+
+  if isempty(des{i})
+    if ~irrev_geq
+      matching_mcs(i)= isequal(mcs{i}, mcs3{i});
+    end
+  else
+    if irrev_geq
+      matching_mcs(i)= isequal(mcs{i}, mcs2{i});
+    else
+      matching_mcs(i)= isequal(mcs{i}, mcs2{i}) && isequal(mcs{i}, mcs3{i}) && isequal(mcs{i}, mcs4{i});
+    end
+  end
+  
+  save(sprintf('%s_s%d_%d_%d.mat', input_file, seed, idx(first), idx(last)), 'idx', 'matching_mcs',...
+    'mcs', 'comp_time', 'mcs2', 'comp_time2', 'mcs3', 'comp_time3', 'mcs4', 'comp_time4');
+end
+disp('ALL FINISHED');
